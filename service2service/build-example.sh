8e3dcf5ed46d37e4b2486dd29d3fac180cb4fbce
Stefan Kürzeder <stefan.kuerzeder@gmail.com>
2020-05-25 13:45:01 +0200

    Addendum to last commit

--- service2service/build-example.sh
+++ service2service/build-example.sh
@@ -2,7 +2,5 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
-
-docker build -t dapr0acr0prexqb.azurecr.io/actor-go-client:$1 -f client/Dockerfile .
-docker build -t dapr0acr0prexqb.azurecr.io/actor-go-caller:$1 -f caller/Dockerfile .
+docker build -t dapr0acr0prexqb.azurecr.io/actor-go-client:$1 -f $DIR/client/Dockerfile $DIR
+docker build -t dapr0acr0prexqb.azurecr.io/actor-go-caller:$1 -f $DIR/caller/Dockerfile $DIR
